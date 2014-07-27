1d65cd376ce7ca52b7dbed07c69c33ad089e5b75
Iggy Villamar <iggy@ivstudio.com>
2014-07-27 10:09:28 -0700

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,22 @@
+// 'use strict'
+
+var travelApp = angular.module('travelApp', [
+		'ngRoute',
+		'hotelCtrl'
+]);
+
+travelApp.config(['$routeProvider', function($routeProvider) {
+	$routeProvider.
+	when('/hotel', {
+		templateUrl: 'partials/hotel.html',
+		controller:'HotelController'
+	}).
+	when('/details/:hotelId', {
+		templateUrl: 'partials/details.html',
+		controller:'DetailsController'
+	}).
+	otherwise({
+		redirectTo: '/hotel'
+	});
+
+}]);
---
+++ app/js/hotelController.js
@@ -0,0 +1,18 @@
+'use strict'
+
+//creating an array w [$scope, http avoids issues w minification.
+var hotelCtrl = angular.module('hotelCtrl', []);
+
+hotelCtrl.controller('HotelController', ['$scope', '$http', function( $scope, $http ) {
+	$http.get('js/data.json').success(function(data) {
+		$scope.hotels = data;
+		$scope.sortorder = 'guestRating';
+	});
+}]);
+
+hotelCtrl.controller('DetailsController', ['$scope', '$http', '$routeParams', function( $scope, $http, $routeParams ) {
+	$http.get('js/data.json').success(function(data) {
+		$scope.hotels = data;
+		$scope.whichHotel = $routeParams.hotelId;
+	});
+}]);
